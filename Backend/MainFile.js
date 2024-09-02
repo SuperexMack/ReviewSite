26d01d9fa930ec11c70e3d02136058376d1d625e
Mack Walker <146699961+SuperexMack@users.noreply.github.com>
2024-09-02 17:02:24 +0530

    Some Random changes

--- Backend/MainFile.js
+++ Backend/MainFile.js
@@ -7,8 +7,6 @@ require("dotenv").config()
 
 const PORT = process.env.PORT
 
-console.log("Your sever is running on the database " + process.env.DATABASE_URL)
-
 app.listen(PORT , (req,res)=>{
     console.log(`Your server is running on the port Number ${PORT}`)
 })
