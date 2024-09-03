3d6a05ab789ded9c9017928ee6e779d846588f61
Mack Walker <146699961+SuperexMack@users.noreply.github.com>
2024-09-03 21:47:30 +0530

    Day Two minor changes

--- Backend/MainFile.js
+++ Backend/MainFile.js
@@ -8,7 +8,6 @@ let SecretCode = process.env.SECRET_CODE;
 console.log(SecretCode)
 const PORT = process.env.PORT
 
-<<<<<<< HEAD
 app.use(express.json())
 
 
@@ -16,8 +15,7 @@ console.log("Your sever is running on the database " + process.env.DATABASE_URL)
 
 app.use("/v1" , UserAuth)
 
-=======
->>>>>>> 26d01d9fa930ec11c70e3d02136058376d1d625e
+
 app.listen(PORT , (req,res)=>{
     console.log(`Your server is running on the port Number ${PORT}`)
 })
